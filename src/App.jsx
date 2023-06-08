f1619365ea6d804b7cec4eaae85a7c7180571375
Wagner Lima <wagner.sousalima@gmail.com>
2023-06-08 19:00:49 -0300

    Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 
+import styles from "./App.module.css"
 import "./global.css"
+import { Sidebar } from "./components/Sidebar"
 
 
  export function App() {
    return (
      <>
        <Header />
-       
-       <Post author="Wagner Lima" content="uai so trem" />
+
+       <div className={styles.wrapper}>
+         
+         <Sidebar />
+         
+         <main>
+         <Post author="Wagner Lima" content="uai so trem" />
        
        <Post author="Dimas Lima" content="eita nu o du zoro"/>
     
+         </main>
+       </div>
+
+      
     </>
   )
 }
