0f9e8b21abcd63b669f9d9562035e85b94ac0aab
Wagner Lima <wagner.sousalima@gmail.com>
2023-06-09 01:55:39 -0300

    update form com ajustes de data e hora, e uso de state

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,63 @@
-import { Header } from "./components/Header"
-import { Post } from "./components/Post"
-import { Sidebar } from "./components/Sidebar"
+import { Header } from './components/Header';
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar';
 
-import styles from "./App.module.css"
-import "./global.css"
+import styles from './App.module.css';
 
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/WagnerSousaLima.png',
+      name: 'Wagner Lima',
+      role: 'Web Developer @Compass.uol'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no Ignite da Rocketseat um Feed interativo 🚀' },
+      { type: 'link', content: 'github.com/WagnerSousaLima' },
+    ],
+    publishedAt: new Date('2023-05-08 21:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/rocketseat-education.png',
+      name: 'Rocketseat',
+      role: '@Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Dev 👋' },
+      { type: 'paragraph', content: 'A trilha Ignite de ReactJs com certeza vai te levar pro next level🚀' },
+      { type: 'link', content: 'rocketseat.com.br' },
+    ],
+    publishedAt: new Date('2023-05-08 20:00:00'),
+  },
+];
 
 export function App() {
-   return (
-      <>
-       <Header />
+  return (
+    <div>
+      <Header />
 
-       <div className={styles.wrapper}>
-         
-         <Sidebar />
-
-         <main>
-         <Post author="Wagner Lima" content="uai so trem" />
-       
-       <Post author="Dimas Lima" content="eita nu o du zoro"/>
-    
-         </main>
-       </div>
-
-      </>
-    )
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <>
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+                />
+              </>
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
 }
-
-
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,53 +1,76 @@
-import { Avatar } from "./Avatar"
-import { Comment } from "./Comment"
-import styles from "./Post.module.css"
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
-export function Post() {
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar
-                        src="https://github.com/WagnerSousaLima.png"
-                    />
-                    <div className={styles.authorInfo}>
-                        <strong>Wagner Lima</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 
-                <time title="08 de junho as 20h" dateTime="2023-06-08">Publicado há 1h</time>
-            </header>
+import styles from './Post.module.css';
 
-            <div className={styles.content}>
-                
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p><a href="">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>
-                </p>
-                
-            </div>
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
 
-            <form className={styles.comentForm}>
-                <strong>Deixe seu feedback</strong>
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
 
-                <textarea
-                    placeholder="Deixe seu comentário"
-                />
-                <footer>
-                    <button type="submit">Comentar</button>
-                </footer>
-            </form>
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
 
-            <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-            </div>
+  function handleCrateNewComment() {
+    event.preventDefault()
 
-        </article>
-    )
+    setComments([...comments, comments.length + 1]);
+  }
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return <Comment />
+        })}
+      </div>
+    </article>
+  )
 }
